b15aef6ea739044fd1532d4e7e9b49cac1e5681c
me3enov <mezenov.dizy@gmail.com>
2023-02-15 20:57:46 +0700

    TS cut css

--- src/types/css.d.ts
+++ src/types/css.d.ts
@@ -2,3 +2,8 @@ declare module '.*css' {
   const styles: { [key: string]: string };
   export = styles;
 }
+
+declare module '.*less' {
+  const styles: { [key: string]: string };
+  export = styles;
+}
